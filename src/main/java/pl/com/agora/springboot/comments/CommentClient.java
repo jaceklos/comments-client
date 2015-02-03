323635675345cc95e03a32bdeb6876b4dc8da54b
Jacek Los <jacek.los@agora.pl>
2015-02-03 10:31:02 +0100

    modyfing client

--- src/main/java/pl/com/agora/springboot/comments/CommentClient.java
+++ src/main/java/pl/com/agora/springboot/comments/CommentClient.java
@@ -7,13 +7,13 @@ import pl.com.agora.api.client.rest.bind.annotation.RestPathVariable;
 import pl.com.agora.api.client.rest.bind.annotation.RestRequestBody;
 import pl.com.agora.api.client.rest.bind.annotation.RestRequestMapping;
 
-@RestRequestMapping("/comment")
+@RestRequestMapping("/comments")
 public interface CommentClient {
 
 	@RestRequestMapping("/{articleId}")
     public Future<List<Comment>> getComments(@RestPathVariable("articleId") String articleId);
     
     @RestRequestMapping("/putComment")
-    public Future<Comment> load(@RestRequestBody Comment comment);
+    public Future<Comment> putComment(@RestRequestBody Comment comment);
     
 }
--- src/main/java/pl/com/agora/springboot/comments/CommentError.java
+++ src/main/java/pl/com/agora/springboot/comments/CommentError.java
@@ -2,42 +2,27 @@ package pl.com.agora.springboot.comments;
 
 public class CommentError {
 
-    private String articleId;
-    private String comment;
-    private String message;
-
-    public CommentError() {
-    }
-    
-    public CommentError(String articleId, String comment , String message) {
-        super();
-        this.articleId = articleId;
-        this.comment = comment;
-        this.message = message;
-    }
-
-    public String getArticleId() {
-        return articleId;
-    }
-
-    public void setArticleId(String articleId) {
-        this.articleId = articleId;
-    }
-
-    public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-
+	private String message;
+	private Integer errorCode;
+	private boolean result;
 	public String getMessage() {
-        return message;
-    }
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Integer getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(Integer errorCode) {
+		this.errorCode = errorCode;
+	}
+	public boolean isResult() {
+		return result;
+	}
+	public void setResult(boolean result) {
+		this.result = result;
+	}
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
+    
 }
